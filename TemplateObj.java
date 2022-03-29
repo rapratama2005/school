85853fdee4eb91ff5393b99d934bad852d27974d
I'mNotRisky <rapratama2005@gmail.com>
2022-03-29 11:03:11 -0500

    Update TemplateObj.java

--- TemplateObj.java
+++ TemplateObj.java
@@ -4,13 +4,13 @@
 
 public class TemplateObj //remember to change this and the constructor 
 {
-   int x;
-   int y;
+   private int x;
+   private int y;
    //No main method
    
-   public TemplateObj(int initialX, int initialY){ //change this as well
-      x = initialX;
-      y = initialY;
+   public TemplateObj(int x, int y){ //change this as well
+      this.x = x;
+      this.y = y;
    }
    
    public TemplateObj(){ 
