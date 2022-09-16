e57d933ba4f490174d10ac6fa540f6a719f7db02
I'mNotRisky <rapratama2005@gmail.com>
2022-09-16 10:04:15 -0500

    Create OddEvenSetRunner.java

---
+++ OddEvenSetRunner.java
@@ -0,0 +1,20 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Arrays;
+import java.util.Scanner;
+import static java.lang.System.*;
+import java.util.*;
+import java.io.*;
+
+public class OddEvenSetRunner
+{
+
+	public static void Main (String args[]){
+		
+	}
+}
